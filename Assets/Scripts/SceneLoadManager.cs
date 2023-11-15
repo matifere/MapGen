201c88f2ae51391c6ec64946437af6358bac26f9
matfere <34757838+matifere@users.noreply.github.com>
2023-11-15 13:34:20 -0300

    Cambios en el menu de carga

--- Assets/Scripts/SceneLoadManager.cs
+++ Assets/Scripts/SceneLoadManager.cs
@@ -1,29 +1,42 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
-
 
 public class SceneLoadManager : MonoBehaviour
 {
     [SerializeField] private Slider slider;
     [SerializeField] private GameObject loadPanel;
 
+    private float loadTime = 2f; 
+
     public void SceneLoad(int sceneIndex)
     {
         loadPanel.SetActive(true);
         StartCoroutine(LoadAsync(sceneIndex));
     }
+
     IEnumerator LoadAsync(int sceneIndex)
     {
         AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneIndex);
-        while (!asyncOperation.isDone)
+        asyncOperation.allowSceneActivation = false;
+
+        float timer = 0f;
+
+        while (timer < loadTime)
         {
-            Debug.Log(asyncOperation.progress);
-            slider.value = asyncOperation.progress;
+            timer += Time.deltaTime;
+
+            float progress = Mathf.Clamp01(timer / loadTime);
+            Debug.Log(progress);
+
+            float smoothValue = Mathf.Lerp(slider.value, progress, Time.deltaTime * 5f);
+            slider.value = smoothValue;
+
             yield return null;
         }
+
+        
+        asyncOperation.allowSceneActivation = true;
     }
 }
